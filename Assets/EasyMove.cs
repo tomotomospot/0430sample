9b0928ad0e9e97c215742866d846d2cc40760c09
tomotomospot <kinematoron_sas@yahoo.co.jp>
2019-04-25 09:05:16 +0900

    first commit

---
+++ Assets/EasyMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasyMove : MonoBehaviour
+{
+    public float speed = 2.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.position -= transform.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+    }
+}
